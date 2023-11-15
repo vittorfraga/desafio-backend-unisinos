0c0d2329da63ebdd739e97c1f8ebd1bdfa84e40d
Vinicius Hugenthobler <vinicius.hugenthobler@nubank.com.br>
2023-11-15 15:36:31 -0300

    Created repository, controller and service for route /class/list, and its subroutes

--- src/repository/classesRepository.js
+++ src/repository/classesRepository.js
@@ -71,6 +71,31 @@ class ClassesRepository {
 
     return result.rows[0];
   }
+
+  async listAllClasses() {
+    const query = `SELECT * FROM classes`;
+    const result = await clientDb.query(query, [id]);
+
+    return result.rows;
+  }
+
+  async listClassesByTeacherId(teacherId) {
+    const query = `SELECT * FROM classes AS c join users AS u on c.professor_id = u.id WHERE u.id = $1`;
+    const result = await clientDb.query(query, [teacherId]);
+
+    return result.rows;
+  }
+
+  async listClassesByStudentId(studentId) {
+    const query = `SELECT * 
+    FROM enrollment AS e 
+    JOIN classes AS c ON e.classes_id = c.id
+    JOIN users AS e.student_id = u.id
+    WHERE u.id = $1`;
+    const result = await clientDb.query(query, [studentId]);
+
+    return result.rows;
+  }
 }
 
 module.exports = ClassesRepository;
